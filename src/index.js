ad74261afadd84f65d7c94352175b25cd29bb22e
Adith <adithwidyapradipta@gmail.com>
2017-08-22 16:43:04 +0700

    Masih belajar pelan2 react basicnya, dan API.

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+// API key from Google to use API
+const API_KEY = 'AIzaSyBjEmYdKyD8VaavuLFJsCwr_pNAC2lt9PY';
+
+// Create a new component.
+// This component should produce some HTML
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: []	
+		};
+
+		// Example fetch
+		YTSearch({key: API_KEY, term: 'shooting stars'}, (videos) => {
+			this.setState({ videos }); // ES6 new way
+			// this.setState({ videos: videos }); // same as this 
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
+}
+
+// Take this component's generated HTML and put it
+// on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
